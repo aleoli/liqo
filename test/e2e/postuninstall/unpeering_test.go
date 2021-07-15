6792f24bcd04f70861adac173bea979d44537c36
Alex Palesandro <alex.palesandro@gmail.com>
2021-07-15 12:57:08 +0200

    Upgrade E2E Tests

--- test/e2e/postuninstall/unpeering_test.go
+++ test/e2e/postuninstall/unpeering_test.go
@@ -1,18 +1,4 @@
-// Copyright 2019-2021 The Liqo Authors
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//      http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
-package unjoine2e
+package unjoin_e2e
 
 import (
 	"context"
@@ -31,17 +17,7 @@ import (
 	"github.com/liqotech/liqo/test/e2e/testutils/tester"
 )
 
-const (
-	// clustersRequired is the number of clusters required in this E2E test.
-	clustersRequired = 2
-	// controllerClientPresence indicates if the test use the controller runtime clients.
-	controllerClientPresence = false
-	// testName is the name of this E2E test.
-	testName = "E2E_UNJOIN"
-)
-
 func Test_Unjoin(t *testing.T) {
-	util.CheckIfTestIsSkipped(t, clustersRequired, testName)
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "Liqo E2E Suite")
 }
@@ -82,7 +58,7 @@ func NoPods(clientset *kubernetes.Clientset, namespace string) error {
 		return err
 	}
 	if len(pods.Items) > 0 {
-		return fmt.Errorf("there are still running pods in Liqo namespace")
+		return fmt.Errorf("There are still running pods in Liqo namespace")
 	}
 	return nil
 }
@@ -97,7 +73,7 @@ func NoJoined(clientset *kubernetes.Clientset) error {
 	}
 
 	if len(nodes.Items) > 0 {
-		return fmt.Errorf("there are still virtual nodes in the cluster")
+		return fmt.Errorf("There are still virtual nodes in the cluster")
 	}
 	return nil
 
